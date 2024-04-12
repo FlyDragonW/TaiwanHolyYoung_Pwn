4dc5952aca8735985054df3e0f17b666090b2074
FlyDragonW <96608738+FlyDragonW@users.noreply.github.com>
2024-04-12 13:02:05 +0800

    Add files via upload

---
+++ main/pwn_2/source_bof_var.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//gcc -o bof_var bof_var.c -fno-stack-protector
+
+int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    int key = 1234;
+
+    puts("Please enter your name:");
+    char name[16];
+    read(0, name, 100);
+
+    if(key == 0xfaceb00c){
+        puts("flag{0v3rf1oW!}");
+    }
+    return 0;
+}
